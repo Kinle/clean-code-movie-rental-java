91bb29903fc04a90c74ce426ec49661be2010a4e
Kinle A G <kinle.greshka@thoughtworks.com>
2021-08-23 11:29:33 +0530

    [Kinle][Day 1] Extract frequent renter points - Long method

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -25,13 +25,8 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
         for (Rental each : rentals) {
             totalAmount += each.amount();
-            
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+
+            frequentRenterPoints += frequentRenterPoints(each);
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -43,6 +38,16 @@ public class Customer {
         result += "You earned " + frequentRenterPoints
                 + " frequent renter points";
         return result;
+    }
+
+    private int frequentRenterPoints(Rental each) {
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                &&
+                each.getDaysRented() > 1) frequentRenterPoints++;
+        return frequentRenterPoints;
     }
 
 }
