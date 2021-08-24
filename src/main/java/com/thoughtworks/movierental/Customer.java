12d176ad4e67823576ae3849ad627a5b88a3a8f4
Kinle A G <kinle.greshka@thoughtworks.com>
2021-08-24 14:46:30 +0530

    [Kinle][Day 1] Implement HTML Statement and Characterization test

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -34,6 +34,23 @@ public class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        String result = "<h3>Rental Record for " + getName() + "<h3>";
+        result += "<p>";
+        for (Rental each : rentals) {
+            //show figures for this rental
+            result += each.getMovie().getTitle() + "<b>" +
+                    each.amount() + "</b><br/>";
+        }
+        result += "</p>";
+
+        //add footer lines result
+        result += "<p>Amount owed is <b>" + totalAmount() + "</b></p>";
+        result += "<p>You earned <b>" + totalFrequentRenterPoints()
+                + "</b> frequent renter points</p>";
+        return result;
+    }
+
     private int totalFrequentRenterPoints() {
         return rentals.stream().mapToInt(Rental::frequentRenterPoints).sum();
     }
--- src/test/java/com/thoughtworks/movierental/CustomerTests.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTests.java
@@ -23,4 +23,22 @@ class CustomerTest {
                 "Amount owed is 51.5\n" +
                 "You earned 4 frequent renter points");
     }
+
+    @Test
+    public void shouldGenerateHtmlStatement() {
+        Customer customer = new Customer("John Doe");
+
+        customer.addRental(new Rental(new Movie("Jumanji", Movie.CHILDRENS), 2));
+        customer.addRental(new Rental(new Movie("3 idiots", Movie.REGULAR), 4));
+        customer.addRental(new Rental(new Movie("Avengers", Movie.NEW_RELEASE), 15));
+
+        String statement = customer.htmlStatement();
+
+        assertEquals(statement, "<h3>Rental Record for John Doe<h3>" +
+                "<p>Jumanji<b>1.5</b><br/>" +
+                "3 idiots<b>5.0</b><br/>" +
+                "Avengers<b>45.0</b><br/></p>" +
+                "<p>Amount owed is <b>51.5</b></p>" +
+                "<p>You earned <b>4</b> frequent renter points</p>");
+    }
 }
